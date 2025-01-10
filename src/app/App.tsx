160d6f97586a49eb4bc614e771f8ad27271c783e
Meet <imailmeet4000@gmail.com>
2025-01-10 18:33:46 +0530

    working

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import "../../global.css"; // Make sure this is required and set up correctly for Expo
+
+
+
+export default function App() {
+  
+
+  return (
+    <SafeAreaView >
+      <View style={{ height: '100%', width: 40, backgroundColor: 'slategray' }}>
+        <Text>hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
---
+++ src/app/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+
+const Login = () => {
+  console.log('login page')
+  return (
+    <SafeAreaView>
+         <View style={{ height: '100%', width: 40, backgroundColor: 'slategray' }}>
+        <Text>hello</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Login
